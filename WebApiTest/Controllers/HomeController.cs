3961bf089af530d7bf6bcc6ae9785e1c23146ac3
sennen.thompsonsmith@gmail.com <sennen.thompsonsmith@gmail.com>
2023-03-03 12:24:07 +0000

    Add project files.

---
+++ WebApiTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(); //http://localhost:63400/api/products - load own localhost numbers
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
